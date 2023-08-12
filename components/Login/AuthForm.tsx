0945e93863cdd1600fb739d4409c991b1c206944
eyucoder <eyueldaniel0921@gmail.com>
2023-08-12 19:38:11 +0300

    callback url

--- components/Login/AuthForm.tsx
+++ components/Login/AuthForm.tsx
@@ -1,15 +1,15 @@
 'use client';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import supabase from '@/lib/supabase';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 export default function AuthForm() {
-  const supabase = createClientComponentClient();
+  const callbackUrl = window?.location.host + '/auth/callback';
 
   return (
     <div className='mb-6'>
       <Auth
-        redirectTo='https://aicolor.vercel.app/auth/callback'
+        redirectTo={callbackUrl}
         supabaseClient={supabase}
         view='magic_link'
         showLinks={false}
